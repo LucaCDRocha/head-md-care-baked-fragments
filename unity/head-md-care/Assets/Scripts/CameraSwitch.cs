d047655f69fd2ab224c742b202da4d2d7c1f0eeb
LucaCDRocha <lucadrocha@gmail.com>
2026-05-20 14:32:14 +0200

    Refactor camera management and update project settings

--- unity/head-md-care/Assets/Scripts/CameraSwitch.cs
+++ unity/head-md-care/Assets/Scripts/CameraSwitch.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using UnityEngine;
 // use new input system
 using UnityEngine.EventSystems;
@@ -9,18 +8,57 @@ public class CameraSwitch : MonoBehaviour, IPointerClickHandler
     // take in parameter x cameras to switch between, if empty, switch between all cameras in the scene
     public Camera[] cameras;
 
-    //detect a mouse click
-    public void OnPointerClick(PointerEventData eventData)
+    public void Start()
     {
-        // ensure we have a valid camera array; if inspector array is empty or contains only nulls, find scene cameras
-        if (cameras == null || cameras.Length == 0 || cameras.All(c => c == null))
+        // if inspector array is empty or contains only nulls, find scene cameras
+        bool allNull = true;
+        if (cameras == null || cameras.Length == 0)
         {
-            cameras = FindObjectsOfType<Camera>();
+            allNull = true;
+        }
+        else
+        {
+            allNull = true;
+            for (int i = 0; i < cameras.Length; i++)
+            {
+                if (cameras[i] != null)
+                {
+                    allNull = false;
+                    break;
+                }
+            }
+        }
+
+        if (allNull)
+        {
+            cameras = FindObjectsByType<Camera>(FindObjectsSortMode.InstanceID);
         }
 
         // filter out any null entries
-        cameras = cameras.Where(c => c != null).ToArray();
+        if (cameras != null)
+        {
+            var list = new System.Collections.Generic.List<Camera>();
+            foreach (var c in cameras)
+            {
+                if (c != null) list.Add(c);
+            }
+            cameras = list.ToArray();
+        }
 
+        // disable all but the first camera
+        for (int i = 1; i < cameras.Length; i++)
+        {
+            cameras[i].enabled = false;
+        }
+        // after cameras = FindObjectsByType<Camera>(FindObjectsSortMode.InstanceID);
+        foreach (var c in cameras) c.targetDisplay = 0; // Display 1 is index 0
+    }
+
+    //detect a mouse click
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // mark parameter as used to avoid unused-parameter warnings
+        _ = eventData;
         if (cameras.Length == 0)
         {
             return;
